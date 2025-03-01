5f5e1f61c641e4c601253bf83d6176fc2f48cabb
Muhammad Masood <m.masood7679@gmail.com>
2025-03-02 04:59:35 +0500

    topaz: Welcome LineageOS
    Add Common Lunch Choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_topaz.mk
+    $(LOCAL_DIR)/lineage_topaz.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_topaz-user \
+    lineage_topaz-userdebug \
+    lineage_topaz-eng
