d44c58f825cabe93b94173a34169d8786f450a44
karthick-kk <kkzone@gmail.com>
2025-03-03 09:49:43 -0300

    topaz: Workaround for voip audio routing issue using a monitoring service
    Fixes the problem of no sound during a VoIP call on speaker, earpiece.
    This fix is intended as an interim solution pending a proper resolution in the upstream audio stack.
    - When changing the sound output, for example from the speaker to the regular one, the sound in the call disappears completely, this should be fixed as of now.

--- device.mk
+++ device.mk
@@ -444,7 +444,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Device-specific settings
  PRODUCT_PACKAGES += \
-     XiaomiParts
+    XiaomiParts \
+    XiaomiVoIPFix
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
